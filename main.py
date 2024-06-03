8970a878a81a38be24f7a0c973275bdcae84b8b1
gcroci2 <crocioni.giulia@gmail.com>
2024-06-03 17:41:35 +0200

    update stable_baseline to stable_baseline3

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-import gym
-
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO2
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3 import PPO
 
 from env.StockTradingEnv import StockTradingEnv
 
 import pandas as pd
 
 df = pd.read_csv('./data/MSFT.csv')
+mode = 'live'
+
 df = df.sort_values('Date')
 
 # The algorithms require a vectorized environment to run
-env = DummyVecEnv([lambda: StockTradingEnv(df)])
+# DummyVecEnv needs a function that returns an environment
+# as an argument
+# Stable Baselines algorithms expect the environment to be
+# vectorized for consistency. Even if you are not using parallel
+# environments (which would require a vectorized setup with
+# multiple environments), using DummyVecEnv ensures that your
+# single environment adheres to the expected interface.
+# This is particularly useful for algorithms that are designed
+# to take advantage of parallelism but need to work with a single
+# environment during development or for simpler tasks.
+env = DummyVecEnv([lambda: StockTradingEnv(df, render_mode=mode)])
 
-model = PPO2(MlpPolicy, env, verbose=1)
+model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=50)
+model.save("ppo_test")
+
+del model # remove to demonstrate saving and loading
+
+model = PPO.load("ppo_test")
 
 obs = env.reset()
 for i in range(len(df['Date'])):
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
-    env.render(title="MSFT")
+    env.render(mode=mode)
